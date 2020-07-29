3602665fdc76a48c73d83672f2e91a3a5cb8a1b3
João Vitor <joao.vtr.cardoso@gmail.com>
2020-07-29 14:03:30 -0300

    criacao de rotas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Empresa;
+use App\Funcionario;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +14,55 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+ 
+// Empresas
+Route::get('empresas', function() {
+    return Empresa::all();
+});
+ 
+Route::get('empresas/{id}', function($id) {
+    return Empresa::find($id);
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('empresas', function(Request $request) {
+    return Empresa::create($request->all());
+});
+
+Route::put('empresas/{id}', function(Request $request, $id) {
+    $empresa = Empresa::findOrFail($id);
+    $empresa->update($request->all());
+
+    return $empresa;
+});
+
+Route::delete('empresas/{id}', function($id) {
+    Empresa::find($id)->delete();
+
+    return 204;
+});
+
+// Funcionarios
+Route::get('funcionarios', function() {
+    return Funcionario::all();
+});
+ 
+Route::get('funcionarios/{id}', function($id) {
+    return Funcionario::find($id);
+});
+
+Route::post('funcionarios', function(Request $request) {
+    return Funcionario::create($request->all());
+});
+
+Route::put('funcionarios/{id}', function(Request $request, $id) {
+    $funcionario = Funcionario::findOrFail($id);
+    $funcionario->update($request->all());
+
+    return $funcionario;
+});
+
+Route::delete('funcionarios/{id}', function($id) {
+    Funcionario::find($id)->delete();
+
+    return 204;
 });
