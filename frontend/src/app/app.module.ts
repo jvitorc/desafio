5e84e13a682653447ebe4c91c78950c675df11a9
João Vitor <joao.vtr.cardoso@gmail.com>
2020-07-30 12:20:32 -0300

    footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule, MatToolbar }from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
