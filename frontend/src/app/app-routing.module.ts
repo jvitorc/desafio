7afd29fdf226c12132e417fe3768de20fa17ccf9
João Vitor <joao.vtr.cardoso@gmail.com>
2020-07-30 12:43:45 -0300

    criando rotas basicas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { FuncionariosCrudComponent } from './views/funcionarios-crud/funcionarios-crud.component';
+import { EmpresasCrudComponent } from './views/empresas-crud/empresas-crud.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "empresas",
+    component: EmpresasCrudComponent
+  },
+  {
+    path: "funcionarios",
+    component: FuncionariosCrudComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
