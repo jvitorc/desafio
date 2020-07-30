a3676704b3f26d331bc176fc9e00fa8b2b38aff3
João Vitor <joao.vtr.cardoso@gmail.com>
2020-07-30 13:59:59 -0300

    model empresa

---
+++ frontend/src/app/components/empresas/empresa.model.ts
@@ -0,0 +1,10 @@
+export interface Empresa {
+    id?: number
+    nome?: string
+    cnpj?: string 
+    endereco?: string
+    created_at?: string
+    updated_at?: string
+    funcionarios?: any
+    pivot?:any
+}
